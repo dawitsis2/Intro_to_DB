096947608eeaa1f1dbfd2af39a6dabfd7177ae1e
Dawit Sisay <dawitsisay42@gmail.com>
2025-02-01 00:13:01 -0800

    Created task_3.sql with all required tables

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,7 @@
--- List all tables in the alx_book_store database
+-- List all tables in the alx_book_store da
+-- Select the database
+USE alx_book_store;
+
+-- List all tables in the selected database
 SHOW TABLES;
+
