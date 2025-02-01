3e6d33e56da19a473b398e50d714a10b6aebfb56
Dawit Sisay <dawitsisay42@gmail.com>
2025-02-01 00:19:18 -0800

    Created task_4.sql with all required tables

--- task_4.sql
+++ task_4.sql
@@ -2,6 +2,6 @@
 USE alx_book_store;
 
 -- Get the full description of the "books" table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, CHARACTER_MAXIMUM_LENGTH
-FROM information_schema.columns
-WHERE table_name = 'Books' AND table_schema = 'alx_book_store';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
