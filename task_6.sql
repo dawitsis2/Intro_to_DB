8d10c254558a26baeeadfa9e7bcb9c73ba121fd9
Dawit Sisay <dawitsisay42@gmail.com>
2025-02-03 06:11:59 -0800

    Created task_6.sql with all required tables

--- task_6.sql
+++ task_6.sql
@@ -1,52 +1,7 @@
--- Create Database
-CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- Create Authors Table
-CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
-    author_name VARCHAR(215) NOT NULL
-);
-
--- Create Books Table
-CREATE TABLE IF NOT EXISTS Books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
-);
-
--- Create Customers Table
-CREATE TABLE IF NOT EXISTS customer (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
-    address TEXT NOT NULL
-);
-
--- Create Orders Table
-CREATE TABLE IF NOT EXISTS Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
-
--- Create Order_Details Table
-CREATE TABLE IF NOT EXISTS Order_Details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
-
--- Insert Customers Data
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
-(`2`, `Blessing Malik`, `bmalik@sandtech.com`, `124 Happiness Ave.`),
-(`3`, `Obed Ehoneah`, `eobed@sandtech.com`, `125 Happiness Ave.`),
-(`4`, `Nehemial Kamolu`, `nkamolu@sandtech.com`, `126 Happiness Ave.`);
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
