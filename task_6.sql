512865ac8f23082326cd5c974742a4563cb90ad1
Dawit Sisay <dawitsisay42@gmail.com>
2025-02-03 05:53:59 -0800

    Created task_6.sql with all required tables

--- task_6.sql
+++ task_6.sql
@@ -45,7 +45,8 @@ CREATE TABLE IF NOT EXISTS Order_Details (
 );
 
 -- Insert Customers Data
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+(`2`, `Blessing Malik`, `bmalik@sandtech.com`, `124 Happiness Ave.`),
+(`3`, `Obed Ehoneah`, `eobed@sandtech.com`, `125 Happiness Ave.`),
+(`4`, `Nehemial Kamolu`, `nkamolu@sandtech.com`, `126 Happiness Ave.`);
